b8b05365a3be2ec8537cb3ed98ff8da5330bb031
Keith Bamford <keithbamford@keiths-macbook-pro.local>
2016-08-31 12:05:24 +0100

    added Tournee model plus extra Golfer attributes

--- TuesdayGolfClub/Golfer+CoreDataProperties.swift
+++ TuesdayGolfClub/Golfer+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Golfer+CoreDataProperties.swift
 //  TuesdayGolfClub
 //
-//  Created by Keith Bamford on 30/08/2016.
+//  Created by Keith Bamford on 31/08/2016.
 //  Copyright © 2016 AKA Consultants. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -15,5 +15,10 @@ import CoreData
 extension Golfer {
 
     @NSManaged var name: String?
+    @NSManaged var firstName: String?
+    @NSManaged var surname: String?
+    @NSManaged var clubHandicap: NSDecimalNumber?
+    @NSManaged var tuesdayHandicap: NSNumber?
+    @NSManaged var playingHandicap: NSNumber?
 
 }
